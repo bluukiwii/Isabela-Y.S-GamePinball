e021e0254cd72befc3cc367b203e757600cdd30d
bluukiwii <isabelayesamine2310@gmail.com>
2024-09-24 13:30:50 +0700

    Launcher

--- Assets/Script/LauncherController.cs
+++ Assets/Script/LauncherController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class LauncherController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Collider bola;
+    public KeyCode input;
+
+    public float maxTimeHold;
+    public float maxForce;
+    
+    private bool isHold = false;
+
+    private void OnCollisionStay(Collision collision)
     {
-        
+        if (collision.collider == bola)
+        {
+            ReadInput(bola);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReadInput(Collider collider)
     {
+        if (Input.GetKey(input) && !isHold)
+        {
+            StartCoroutine(StartHold(collider));
+        }
+    }
+
+    private IEnumerator StartHold(Collider collider)
+    {
+        isHold = true;
+
+        float force = 0.0f;
+        float timeHold = 0.0f;
+
+        while (Input.GetKey(input))
+        {
+            force = Mathf.Lerp(0, maxForce, timeHold/maxTimeHold);
+
+            yield return new WaitForEndOfFrame();
+            timeHold += Time.deltaTime;
+        }
+
+        collider.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        isHold = false;
         
     }
 }
